9b5cf53394f2299955a3af121354a2f649ae9739
GayendraSHK <kaveeshagayendra2@gmail.com>
2024-09-10 18:27:17 +0530

    Refactor Hero component CSS and JSX

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,36 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
-//import React from 'react'
+import React, { useRef, useEffect } from 'react';
 import './Hero.css'
 import profile_img from '../../assets/profile_img2.png'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import Typed from 'typed.js'; // Import the 'Typed' library
 
 const Hero = () => {
+
+  const typedRef = useRef(null);
+  useEffect(() => {
+    const options = {
+      strings: [
+        "Full Stack Developer.",
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    };
+
+    const typed = new Typed(typedRef.current, options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div id='home' className='hero'>
         <img src={profile_img} alt="" />
-        <h1><span>I'm Kaveesha Gayendra,</span> Full Stack Developer.</h1>
+        <span>I'm Kaveesha Gayendra,</span>
+        <h1 ref={typedRef}></h1>
         {/* <p>
           I am a frontend developer from California, USA with 10 years of experience in 
           multiple companies like Microsoft, Tesla and Apple.
