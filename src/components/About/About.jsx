6ef5058f3cfc123e00e9f0480d9d1d4742fe2785
GayendraSHK <kaveeshagayendra2@gmail.com>
2024-09-11 17:26:01 +0530

    Update Technologies section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
             strings: [
                 "I am a highly motivated and enthusiastic IT undergraduate at the University of Moratuwa, driven by a passion for technology and a commitment to excellence. With a solid academic background and strong technical skills, I thrive in dynamic environments where I can tackle challenges and continuously grow.<br /><br />Eager to expand my knowledge and stay at the forefront of emerging technologies, I am dedicated to contributing to innovative, impactful projects. Whether working independently or as part of a team, I focus on delivering high-quality results and making a meaningful impact in the ever-evolving world of technology."
             ],
-            typeSpeed: 5,
+            typeSpeed: 30,
             backSpeed: 0,
             loop: false,
             showCursor: false, // Disable the blinking cursor
